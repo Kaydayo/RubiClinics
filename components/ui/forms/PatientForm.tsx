4110c9b1d331e71f3a5928152f680aea8d4ed9b7
Adebowale Mujeeb <89817452+Kaydayo@users.noreply.github.com>
2024-07-14 20:06:55 +0100

    "Updated PatientForm to call createUser action and handled user creation with Appwrite API"

--- components/ui/forms/PatientForm.tsx
+++ components/ui/forms/PatientForm.tsx
@@ -14,6 +14,7 @@ import SubmitButton from "../../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "../../../lib/validation"
 import {useRouter} from 'next/navigation'
+import { createUser } from "../../../lib/actions/patient.sctions"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -42,9 +43,9 @@ const PatientForm = () => {
     async function onSubmit({name,email,phone}: z.infer<typeof UserFormValidation>) {
         setisLoading(true)
         try {
-            // const userData = {name,email,phone}
-            // const user = await createUser(userData)
-            // if(user) router.push(`/patients/${user.id}/register`)
+            const userData = {name,email,phone}
+            const user = await createUser(userData)
+            if(user) router.push(`/patients/${user?.$id}/register`)
         } catch (error) {
             console.log(error)
         }
